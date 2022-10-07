e83eab1ca3c27550b4023da7d59f38b6e1fdf01a
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-10-07 16:56:29 -0500

    Solucion v2

--- pregunta.py
+++ pregunta.py
@@ -1,33 +1,29 @@
 # Limpieza de datos usando Pandas
 
-from datetime import datetime
 import pandas as pd
-import re
+from datetime import datetime
 
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
     
     # Eliminación de datos faltantes y duplicados
-    df.dropna(axis  =0, inplace = True)
+    df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
 
-    # Poner en minúscula las columnas de tipo texto
+    # Poner en minúscula las columnas de tipo texto y eliminar carácteres especiales de idea_negocio y barrio
     for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
-        df[columna] = df[columna].apply(lambda x: x.lower())
-
-    # Eliminar carácteres especiales de idea_negocio y barrio
-    for columna in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[columna] = df[columna].str.lower()
         df[columna] = df[columna].apply(lambda x: x.replace('_', ' '))
         df[columna] = df[columna].apply(lambda x: x.replace('-', ' '))
 
     # Depurar la columna monto_del_credito quitando los carácteres extraños (No numéricos)
-    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: re.sub("\$[\s*]", "", x))
-    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: re.sub(",", "", x))
-    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: re.sub("\.00", "", x))
-    df['monto_del_credito'] = df['monto_del_credito'].apply(int)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
+    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
     
     # Corregir tipo de dato de la columna comuna_ciudadano
-    df['comuna_ciudadano'] = df['comuna_ciudadano'].apply(float)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
 
     # Corregir tipo de dato de la columna comuna_ciudadano -> Pasar a formato de fecha
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
